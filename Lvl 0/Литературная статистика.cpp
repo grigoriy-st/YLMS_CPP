f5b09b9e479b3639cf79452ff601274304683a9c
Grigoriy <gregorey060@gmail.com>
2025-07-01 07:32:00 +0600

    SOme edits

--- Lvl 0/Литературная статистика.cpp
+++ Lvl 0/Литературная статистика.cpp
@@ -1,55 +1,32 @@
 #include <iostream>
-#include <utility>
 #include <map>
 #include <string>
-#include <sstream>
+#include <algorithm>
 
 using namespace std;
 
-pair<string, int> getValuesFromLine(string line) {
-    string genre;
-    string quantityOfPages;
-    bool firstMinusIsFinded = false;
-    bool secondMinusIsFinded = false;
-
-    for (int l = 0; l < line.length(); l++) {
-        if (secondMinusIsFinded) {
-            quantityOfPages += line[l];
-        }
-        
-        if (firstMinusIsFinded) {
-            if (line[l] == '-') {
-                secondMinusIsFinded = true;
-            } else {
-                genre += line[l];
-            }
-        }
-        if (l > 0 &&
-            line[l - 1] == '-' && line[l] == ' ') {
-            firstMinusIsFinded = true;
-        }
-        else {
-            continue;
-        }
-    }
-    cout << genre << " " << quantityOfPages << endl;
-    int quantity = stoi(quantityOfPages);
-    return make_pair(genre, quantity);
-}
-
 int main() {
-    map<string, int> data = {};
+    map<char, int> dict = {
+        {'.', 1}, {',', 1}, {'?', 1}, {'!', 1}, {':', 1},
+        {'A', 2}, {'B', 2}, {'C', 2},
+        {'D', 3}, {'E', 3}, {'F', 3},
+        {'G', 4}, {'H', 4}, {'I', 4},
+        {'J', 5}, {'K', 5}, {'L', 5},
+        {'M', 6}, {'N', 6}, {'O', 6},
+        {'P', 7}, {'Q', 7}, {'R', 7}, {'S', 7},
+        {'T', 8}, {'U', 8}, {'V', 8},
+        {'W', 9}, {'X', 9}, {'Y', 9}, {'Z', 9},
+    };
 
     string line;
     getline(cin, line);
+    
+    transform(line.begin(), line.end(), line.begin(), ::toupper);
 
-    string genre;
-    int quantityOfPages;
-    // auto [genre, quantityOfPages] = getValuesFromLine(line);
-    auto result = getValuesFromLine(line);
-    genre = result.first;
-    quantityOfPages = result.second; 
-    cout << genre << " " << quantityOfPages << endl;
-    // cout << title << " " << genre << " " << quantityOfPages;
+    for (char let: line) {
+        for (int i = 0; i < dict[let]; i++) {
+            cout << dict[let];
+        }
+    }
     return 0;
 }
